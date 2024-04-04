3cdcfbbd74e853a643a343459a3cc9010d674d31
arifbot084 <hanifb024@gmail.com>
2024-04-05 02:36:23 +0530

    subnet modified-5

--- main.tf
+++ main.tf
@@ -18,3 +18,19 @@ resource "aws_subnet" "private_subnet" {
     Name = "private_subnet_${count.index}"
   }
 }
+
+# Create additional subnets if all availability zones are assigned
+locals {
+  assigned_azs = [for subnet in aws_subnet.private_subnet : subnet.availability_zone]
+  remaining_azs = setsubtract(data.aws_availability_zones.available.names, local.assigned_azs)
+}
+
+resource "aws_subnet" "private_subnet_additional" {
+  count             = length(local.remaining_azs)
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = element(var.subnet_cidr_blocks, count.index + length(data.aws_availability_zones.available.names))
+  availability_zone = element(local.remaining_azs, count.index)
+  tags = {
+    Name = "private_subnet_additional_${count.index}"
+  }
+}
--- variable.tf
+++ variable.tf
@@ -6,5 +6,5 @@ variable "availability_zones" {
 variable "subnet_cidr_blocks" {
   description = "A list of CIDR blocks"
   type        = list(string)
-  default     = ["10.10.60.0/24", "10.10.70.0/24", "10.10.80.0/24", "10.10.90.0/24", "10.10.110.0/24", "10.10.120.0/24", "10.10.130.0/24", "10.10.140.0/24", "10.10.150.0/24"]  # You can provide default values here
+  default     = ["10.10.160.0/24", "10.10.170.0/24", "10.10.180.0/24", "10.10.190.0/24", "10.10.110.0/24", "10.10.120.0/24", "10.10.130.0/24", "10.10.140.0/24", "10.10.150.0/24"]  # You can provide default values here
 }
