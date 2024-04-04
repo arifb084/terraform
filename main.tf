a19b32975ccb941991e10c55d7b6e97612efd5f8
arifbot084 <hanifb024@gmail.com>
2024-04-05 02:05:42 +0530

    subnet added

--- main.tf
+++ main.tf
@@ -9,3 +9,11 @@ resource "aws_vpc" "my_vpc" {
   }
 }
 
+resource "aws_subnet" "private_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "10.10.1.0/24"
+  availability_zone = "us-east-1a"
+  tags = {
+    Name = "private_subnet"
+  }
+}
