180995619cd2205896075f646f3e85a816bbc184
arifb084 <166062657+arifb084@users.noreply.github.com>
2024-04-15 10:39:43 +0530

    subnet three and four added.

--- main.tf
+++ main.tf
@@ -8,3 +8,18 @@ resource "aws_vpc" "my_vpc" {
     Name = "my_vpc"
   }
 }
+resource "aws_subnet" "subnet_3" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.10.3.0/24"
+  tags = {
+    Name = "subnet_3"
+  }
+}
+
+resource "aws_subnet" "subnet_4" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.10.4.0/24"
+  tags = {
+    Name = "subnet_4"
+  }
+}
