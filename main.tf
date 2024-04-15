891340980200afa43ec150b0244e4130ada70e7e
arifbot084 <hanifb024@gmail.com>
2024-04-15 10:36:59 +0530

    subnet add

--- main.tf
+++ main.tf
@@ -8,3 +8,18 @@ resource "aws_vpc" "my_vpc" {
     Name = "my_vpc"
   }
 }
+resource "aws_subnet" "subnet_1" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.10.1.0/24"
+  tags = {
+    Name = "subnet_1"
+  }
+}
+
+resource "aws_subnet" "subnet_2" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.10.2.0/24"
+  tags = {
+    Name = "subnet_2"
+  }
+}
